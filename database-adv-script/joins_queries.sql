d68e63d9dd9940346d7a75430a1b5701f964d733
wondavibes <amosayemoro2001@gmail.com>
2025-11-03 14:36:40 +0100

    join queries added

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,38 @@
+SELECT 
+    Booking.booking_id,
+    Booking.start_date,
+    Booking.end_date,
+    Booking.total_price,
+    Booking.status,
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    User.email
+FROM Booking
+INNER JOIN User ON Booking.user_id = User.user_id;
+
+
+
+SELECT 
+    Property.property_id,
+    Property.name,
+    Property.location,
+    Review.review_id,
+    Review.rating,
+    Review.comment,
+    Review.created_at
+FROM Property
+LEFT JOIN Review ON Property.property_id = Review.property_id;
+
+
+SELECT 
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    Booking.booking_id,
+    Booking.start_date,
+    Booking.end_date,
+    Booking.total_price,
+    Booking.status
+FROM User
+FULL OUTER JOIN Booking ON User.user_id = Booking.user_id;
